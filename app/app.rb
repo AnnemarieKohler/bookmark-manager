18228a8d2d422f3cabd9f82599aec1fe51e4aa95
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-24 17:16:34 +0000

    Added feature to have prefilled username and email, after password mismatch

--- app/app.rb
+++ app/app.rb
@@ -13,23 +13,23 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/sign_up'
   end
 
   post '/users' do
-    user = User.create(
+    @user = User.create(
       username: params[:username],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     )
-
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect to '/links'
     else
       flash[:error] = 'Password and confirmation password do not match'
-      redirect to '/users/new'
+      erb :'/users/sign_up'
     end
   end
 
