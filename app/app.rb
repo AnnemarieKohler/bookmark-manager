9910a841cfbf3391b9d4eec6beba696f04842c68
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-24 11:17:29 +0000

    added encryption key

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,7 @@ require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
   enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
   end
@@ -20,7 +21,7 @@ class BookmarkManager < Sinatra::Base
       password_digest: params[:password]
     )
     session[:user_id] = user.id
-    redirect '/links'
+    redirect to '/links'
   end
 
   helpers do
