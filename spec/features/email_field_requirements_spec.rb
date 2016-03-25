2040d4eff6b9145aaec882102f0e2f5cca9ced24
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-25 18:38:22 +0000

    added more feature specs

--- spec/features/email_field_requirements_spec.rb
+++ spec/features/email_field_requirements_spec.rb
@@ -1,9 +1,13 @@
 feature 'Email field' do
   scenario 'cannot be blank' do
-    expect { sign_up(email: '') }.not_to change(User, :count)
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('We need your email address')
   end
 
   scenario 'can\'t sign up with an invalid formatted email address' do
     expect { sign_up(email: "anne-anne.berlin") }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Please make sure to enter your email address in the right format')
   end
 end
